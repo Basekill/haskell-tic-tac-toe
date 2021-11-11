b84871ea6066c6dc4fd378017beea5aad1d5f50d
Chen, Steven <steven.chen121@imperial.ac.uk>
2021-11-11 21:31:03 +0000

    Poorly coded but working game

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -4,10 +4,11 @@ import Data.Char
 import Data.Maybe
 import Data.List
 import Text.Read
+import Debug.Trace
 
 -------------------------------------------------------------------
 data Player = O | X
-            deriving (Eq, Show)
+            deriving (Eq, Show, Enum)
 
 data Cell = Empty | Taken Player
           deriving (Eq, Show)
@@ -62,6 +63,10 @@ diags (cs, n)
   = map (map (cs !!)) [[k * (n + 1) | k <- [0 .. n - 1]],
                       [k * (n - 1) | k <- [1 .. n]]]
 
+constructBoard :: Int -> Board
+constructBoard n
+  = ([Empty | count <- [1..n*n]], n)
+  
 -------------------------------------------------------------------
 
 gameOver :: Board -> Bool
@@ -87,9 +92,16 @@ parsePosition pos
   where
     pos' = '(' : takeWhile (not . isSpace) pos ++ "," ++ dropWhile (not . isSpace) pos ++ ")"
 
+parseSize :: String -> Maybe Int
+parseSize size
+  | n > 0 = Just n 
+  | otherwise = Nothing
+  where
+    n = fromMaybe 0 (readMaybe size :: Maybe Int)
+
 tryMove :: Player -> Position -> Board -> Maybe Board
 tryMove plr (i, j) (cells, n) 
-  | index < 0 || index >= n ^ 2 || (cells !! index) /= Empty = Nothing
+  | i < 0 || j < 0 || i >= n || j >= n || (cells !! index) /= Empty = Nothing
   | otherwise = Just (replace index (Taken plr) cells, n) 
   where
     index = i * n + j
@@ -98,31 +110,71 @@ tryMove plr (i, j) (cells, n)
 -- I/O Functions
 
 prettyPrint :: Board -> IO ()
-prettyPrint
-  = undefined
+prettyPrint board
+  = putStr (concat lines)
+  where
+    lines = [intersperse ' ' (prettyPrint' row) | row <- rows board]
+    prettyPrint' :: [Cell] -> String
+    prettyPrint' []
+      = "\n"
+    prettyPrint' (x : xs)
+      | x == Empty = '-' : prettyPrint' xs
+      | x == Taken O = 'O' : prettyPrint' xs
+      | otherwise = 'X' : prettyPrint' xs
 
 -- The following reflect the suggested structure, but you can manage the game
 -- in any way you see fit.
 
+doParseAction :: (String -> Maybe a) -> String -> IO a
+doParseAction parse errorMsg
+  = do
+       line <- getLine
+       let input = parse line
+       -- need to also check the validity of the given input
+       maybe (do 
+                putStr errorMsg
+                doParseAction parse errorMsg) return input
+
 -- Repeatedly read a target board position and invoke tryMove until
 -- the move is successful (Just ...).
 takeTurn :: Board -> Player -> IO Board
-takeTurn
-  = undefined
-
+takeTurn board plr
+  = do
+                      putStr ("Player " ++ show plr ++ ", make your move (row col): ")
+                      pos <- doParseAction parsePosition "Invalid move, try again: "
+                      let board' = tryMove plr pos board
+                      maybe (do 
+                                putStrLn "Invalid position"
+                                takeTurn board plr) return board'
+    
 -- Manage a game by repeatedly: 1. printing the current board, 2. using
 -- takeTurn to return a modified board, 3. checking if the game is over,
 -- printing the board and a suitable congratulatory message to the winner
 -- if so.
 playGame :: Board -> Player -> IO ()
-playGame
-  = undefined
+playGame board plr
+  = do
+      prettyPrint board 
+      board' <- takeTurn board plr
+      if gameOver board'
+        then do
+               prettyPrint board'
+               putStrLn ("Player " ++ show plr ++ " has won!")
+               putStrLn ("Thank you for playing")
+        else playGame board' (toEnum (((fromEnum plr) + 1) `mod` 2))
+
 
 -- Print a welcome message, read the board dimension, invoke playGame and
 -- exit with a suitable message.
 main :: IO ()
 main
-  = undefined
+  = do 
+      putStrLn "Welcome to tic tac toe on an N x N board"
+      putStr "Enter the board size (N): "
+      n <- doParseAction parseSize "Invalid board size, try again: "
+      let board = constructBoard n 
+      playGame (constructBoard n) X
+
 
 -------------------------------------------------------------------
 
