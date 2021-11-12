ca8d24eda5708505d084c3917fd5bfc3942a7fe3
Chen, Steven <steven.chen121@imperial.ac.uk>
2021-11-12 10:55:10 +0000

    Adds tests for isFull and parseSize

--- Tests.hs
+++ Tests.hs
@@ -9,6 +9,13 @@ gameOverTestCases
      , testBoard3 ==> True
      ]
 
+isFullTestCases
+   = [ testBoard1 ==> False
+     , testBoard2 ==> False
+     , testBoard3 ==> False
+     , testBoard4 ==> True
+     ]
+
 parsePositionTestCases
    = [
        ("0 2") ==> (Just (0,2))
@@ -17,6 +24,23 @@ parsePositionTestCases
      , ("0 %1") ==> (Nothing)
      , ("") ==> (Nothing)
      , ("1 2 3") ==> (Nothing)
+     , ("(0,0)") ==> (Nothing)
+     , ("01 023") ==> (Just (1,23))
+     , ("0000 -012") ==> (Just (0, -12))
+     ]
+
+parseSizeTestCases
+   = [
+       ("1") ==> (Just 1)
+     , ("3") ==> (Just 3)
+     , ("0") ==> (Nothing)
+     , ("-2") ==> (Nothing)
+     , ("0 3") ==> (Nothing)
+     , ("2 1") ==> (Nothing)
+     , ("0 %1") ==> (Nothing)
+     , ("") ==> (Nothing)
+     , ("1 2 3") ==> (Nothing)
+     , ("(0,0)") ==> (Nothing)
      ]
 
 tryMoveTestCases
@@ -28,6 +52,12 @@ tryMoveTestCases
     , (O,(3,3),testBoard1) ==> (Just ([Taken O,Taken X,Empty,Taken O,Taken O,
                                 Empty,Taken X,Taken X,Taken O,Empty,Empty,Taken
                                 X,Taken O,Taken X,Empty,Taken O],4))
+    , (X,(4,4),testBoard3) ==> (Just ([Taken O,Taken X,Empty,Taken O,Taken X,
+                                       Taken O,Empty,Taken X,Taken X,Empty,
+                                       Empty,Empty,Taken X,Taken O,Taken O,
+                                       Taken O,Taken X,Empty,Empty,Taken X,
+                                       Taken X,Empty,Taken O,Empty,Taken X],
+                                       5))
     ]
 
 -- You can add your own test cases above
@@ -36,8 +66,12 @@ allTestCases
   = [
       TestCase "gameOver" (gameOver)
                gameOverTestCases
+    , TestCase "isFull" (isFull)
+               isFullTestCases
     , TestCase "parsePosition" (parsePosition)
                parsePositionTestCases
+    , TestCase "parseSize" (parseSize)
+               parseSizeTestCases
     , TestCase "tryMove" (uncurry3 tryMove)
                tryMoveTestCases
     ]
